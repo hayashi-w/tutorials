93b2da90636aa106fdaf00cf293400fce28a263a
Natsuki Hayashi <32443351+hayashi-w@users.noreply.github.com>
2022-10-09 15:50:59 +0900

    body-multiple-params

---
+++ fastapi/body-multiple-params03.py
@@ -0,0 +1,38 @@
+# Body の特異値
+from fastapi import Body, FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+class User(BaseModel):
+    username: str
+    full_name: str | None = None
+
+
+@app.put("/items/{item_id}")
+# 通常だと importance はクエリパラメータ扱いになるが、「Body」を使うことで　Body 扱いにすることができる
+async def update_item(item_id: int, item: Item, user: User, importance: int = Body()):
+    results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
+    return results
+
+# {
+#     "item": {
+#         "name": "Foo",
+#         "description": "The pretender",
+#         "price": 42.0,
+#         "tax": 3.2
+#     },
+#     "user": {
+#         "username": "dave",
+#         "full_name": "Dave Grohl"
+#     },
+#     "importance": 5
+# }
---
+++ fastapi/body-multiple-params04.py
@@ -0,0 +1,28 @@
+# 単一のボディパラメータを埋め込む
+from fastapi import Body, FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item = Body(embed=True)):
+    results = {"item_id": item_id, "item": item}
+    return results
+
+# Body パラメータが 1 つの時でもキーを指定するようになる
+# {
+#     "item": {
+#         "name": "Foo",
+#         "description": "The pretender",
+#         "price": 42.0,
+#         "tax": 3.2
+#     }
+# }
